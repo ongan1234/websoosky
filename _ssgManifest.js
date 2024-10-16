c2724302c2a1a32ade69f343b48aa7136952bf5d
Pm2112 <163095201+Pm2112@users.noreply.github.com>
2024-10-16 16:36:23 +0700

    first

---
+++ _ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblog","\u002Fblog\u002F[slug]","\u002Fcareers\u002Fculture","\u002Fcareers\u002Fvalues","\u002Fpress","\u002Fteam","\u002Fteam\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
